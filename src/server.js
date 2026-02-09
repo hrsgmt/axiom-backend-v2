14f222821843e3cdf509f768596edac698e647bd
hrsgmt <hrsgmt@gmail.com>
2026-02-09 23:28:37 +0530

    listen detector

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ app.get("/", () => "AXIOM_V2_PIPELINE_TEST_999");
   return "I_AM_V2_CLEAN";
 });
 
+console.log("ABOUT_TO_LISTEN");
 await app.listen({ port: process.env.PORT || 4000, host: "0.0.0.0" });
 
 app.get("/__health", async () => {
